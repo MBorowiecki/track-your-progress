9476841aae19869370075a793b12da0c0cbd31dc
Mateusz Borowiecki <mborowiecki@Mateuszs-MacBook-Pro.local>
2023-05-10 22:30:29 +0200

    feat: basic routing, html, css, js, assets structure

---
+++ public/views/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="public/src/css/main.css">
+    <link rel="stylesheet" href="public/src/css/header.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <title>Track Your Progress - Login</title>
+</head>
+<body>
+    <header class="header">
+        <img src="public/assets/logo.svg" alt="logo" class="header_logo">
+    </header>
+
+    <main class="center-content">
+        <div>lolek</div>
+    </main>
+</body>
+</html>
